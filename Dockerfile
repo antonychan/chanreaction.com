3b96abc34b27c28740638529520ab96d7ba09d65
antonyc <achan0327@gmail.com>
2016-02-07 08:56:52 -0800

    cover

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 FROM ubuntu:14.04
 
-RUN apt-get install -y curl
+RUN apt-get install -y curl git
 
 # install node
 RUN curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 RUN apt-get install -y nodejs=4.2.6-1nodesource1~trusty1
+
+# install nodemon
+RUN npm i nodemon -g
 
 EXPOSE 3000
--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 var koa = require('koa');
 var app = koa();
+var views = require('koa-views');
 
-app.use(function *(next){
-  var start = new Date;
-  console.log("gahhh1")
-  yield next;
-  console.log("gahhh2")
-  var ms = new Date - start;
-  this.set('X-Response-Time', ms + 'ms');
+// Must be used before any router is used
+app.use(views(__dirname + '/views', {
+    extension: 'dust'
+}));
+
+app.use(require('koa-static')(__dirname + '/bower_components'));
+
+// app.use(function *(next){
+//   var start = new Date;
+//   yield next;
+//   var ms = new Date - start;
+//   this.set('X-Response-Time', ms + 'ms');
+// });
+//
+// // logger
+//
+// app.use(function *(next){
+//   var start = new Date;
+//   console.log("gahhh3")
+//   yield next;
+//   console.log("gahhh4")
+//   var ms = new Date - start;
+//   console.log('%s %s - %sms', this.method, this.url, ms);
+// });
+//
+// // response
+//
+app.use(function *() {
+    console.log(this.state)
+    yield this.render('index', {
+      user: 'John'
+    });
 });
 
-// logger
-
-app.use(function *(next){
-  var start = new Date;
-  console.log("gahhh3")
-  yield next;
-  console.log("gahhh4")
-  var ms = new Date - start;
-  console.log('%s %s - %sms', this.method, this.url, ms);
-});
-
-// response
-
-app.use(function *(){
-  this.body = 'Hello World';
-});
 app.listen(3000);
